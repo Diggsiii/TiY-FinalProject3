430c9056aaddcdcde6ba4311be87da5a5e25f2f1
Diggsiii <rd3@me.com>
2016-04-07 14:18:20 -0500

    revert back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['zoomitapp'],
-    :access_key_id => ENV['AKIAIKMUKROSCSKRDHMQ'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
