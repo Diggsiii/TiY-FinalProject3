3bf68b8afd5ee33c08c92ed394b68dcf11257707
Diggsiii <rd3@me.com>
2016-04-08 19:45:26 -0500

    updated about page

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 ruby '2.3.0'
 
- gem 'will_paginate-bootstrap', '~> 1.0.1'
+
+gem 'font-awesome-sass', '~> 4.5.0'
+gem 'will_paginate-bootstrap', '~> 1.0.1'
 gem 'will_paginate', '~> 3.1'
 gem 'aws-sdk', '< 2.0'
 gem 'paperclip', '~> 4.3', '>= 4.3.6'
--- app/controllers/zooms_controller.rb
+++ app/controllers/zooms_controller.rb
@@ -1,63 +1,60 @@
 class ZoomsController < ApplicationController
-  before_action :set_zoom, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-#
-#
-#  # GET /zooms
-#  # GET /zooms.json
+    before_action :set_zoom, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :correct_user, only: [:edit, :update, :destroy]
+    #
+    #
+    #  # GET /zooms
+    #  # GET /zooms.json
 
-def index
-   @zooms = Zoom.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
- end
+    def index
+        @zooms = Zoom.all.order('created_at DESC').paginate(page: params[:page], per_page: 5)
+     end
 
- def show
- end
+    def show
+    end
 
- def new
-   @zoom = current_user.zooms.build
- end
+    def new
+        @zoom = current_user.zooms.build
+    end
 
- def edit
- end
+    def edit
+    end
 
- def create
-   @zoom = current_user.zooms.build(zoom_params)
-   if @zoom.save
-     redirect_to @zoom, notice: 'Zoom was successfully created.'
-   else
-     render action: 'new'
-   end
- end
+    def create
+        @zoom = current_user.zooms.build(zoom_params)
+        if @zoom.save
+            redirect_to @zoom, notice: 'Zoom was successfully created.'
+        else
+            render action: 'new'
+        end
+    end
 
- def update
-  if @zoom.update(zoom_params)
-     redirect_to @zoom, notice: 'Zoom was successfully updated.'
-   else
-     render action: 'edit'
-   end
- end
+    def update
+        if @zoom.update(zoom_params)
+            redirect_to @zoom, notice: 'Zoom was successfully updated.'
+        else
+            render action: 'edit'
+         end
+    end
 
- def destroy
-   @zoom.destroy
-   redirect_to zooms_url
- end
+    def destroy
+        @zoom.destroy
+        redirect_to zooms_url
+    end
 
- private
-   def set_zoom
-     @zoom = Zoom.find(params[:id])
-   end
+    private
 
-   def zoom_params
-     params.require(:zoom).permit(:description, :image, :name)
-   end
+    def set_zoom
+        @zoom = Zoom.find(params[:id])
+    end
 
+    def zoom_params
+        params.require(:zoom).permit(:description, :image, :name)
+    end
 
- def correct_user
-     @zoom = current_user.zooms.find_by(id: params[:id])
-     redirect_to zooms_path, notice: "Not authorized to edit this Zoom" if @zoom.nil?
- end
-
-
-
+    def correct_user
+        @zoom = current_user.zooms.find_by(id: params[:id])
+        redirect_to zooms_path, notice: 'Not authorized to edit this Zoom' if @zoom.nil?
+    end
 end
